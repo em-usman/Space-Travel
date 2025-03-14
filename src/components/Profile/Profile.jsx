6636cdaf3a80645ddf8de01be0d013fe1f333341
imosmantariq@gmail.com <imosmantariq@gmail.com>
2025-03-14 14:43:03 +0500

    make the missions page frontend

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import './Profile.css';
 
 function Profile() {
   return (
-    <div className="container">
+    <div className="profile-container">
       <h1>Profile</h1>
       <p>View and edit your profile details here!</p>
     </div>
